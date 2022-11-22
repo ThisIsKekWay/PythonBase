658f37333f5eb28823230295a6740efea436123b
Koha <BelkoZdorovo@yandex.ru>
2022-11-22 20:41:50 +0300

    Task 2

--- Examples/Pr1.py
+++ Examples/Pr1.py
@@ -1,7 +1,17 @@
-a = int(input('Введите первое число '))
-b = int(input('Введите второе число '))
+###
+# a = int(input('Введите первое число '))
+# b = int(input('Введите второе число '))
+#
+# if (a == b ** 2) or (b == a ** 2):
+#     print('Да')
+# else:
+#     print('Нет')
 
-if (a == b ** 2) or (b == a ** 2):
-    print('Да')
-else:
-    print('Нет')
+# nums = [int(i) for i in input('Введите числа через пробел ').split()]
+# maxx = nums[0]
+# for i in range(1, len(nums)):
+#     if maxx < nums[i]:
+#         maxx = nums[i]
+#
+# print(f'Максимальное число это {maxx}')
+
