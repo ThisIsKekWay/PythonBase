971d9e205c4fe218142191d56237eb5cb612a9cd
Koha <BelkoZdorovo@yandex.ru>
2022-12-11 10:25:58 +0300

    Reworked with List comprehension

--- Homework/HW4/HomeWork4_Ex3.py
+++ Homework/HW4/HomeWork4_Ex3.py
@@ -1,14 +1,9 @@
 # Задайте последовательность чисел.
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 import random
-lst = []
-res_lst = []
+lst = [random.randint(1, 5) for n in range(random.randint(2, 10))]
+res_lst = [n for n in lst if lst.count(n) == 1]
 
-for i in range(random.randint(2, 10)):
-    lst.append(round(random.randint(1, 5), 2))
 print(f'Начальный список: {lst}')
-
-for i in range(len(lst)):
-    if lst.count(lst[i]) == 1:
-        res_lst.append(lst[i])
 print(res_lst)
+
